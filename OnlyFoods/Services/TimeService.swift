42caa52338cf4028a2b6ffc175b8992015586496
Foahh <160589889+Foahh@users.noreply.github.com>
2025-11-21 14:22:50 +0800

    refactor: clean up code formatting and improve padding in RestaurantDetailView

--- OnlyFoods/Services/TimeService.swift
+++ OnlyFoods/Services/TimeService.swift
@@ -10,32 +10,31 @@ import Foundation
 
 class TimeService: ObservableObject {
   @Published var currentTime: Date = Date()
-  
+
   static let shared = TimeService()
-  
+
   private var timer: Timer?
-  
+
   private init() {
     startTimer()
   }
-  
+
   deinit {
     stopTimer()
   }
-  
+
   private func startTimer() {
     // Update immediately
     currentTime = Date()
-    
+
     // Set up timer to update every minute
     timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { [weak self] _ in
       self?.currentTime = Date()
     }
   }
-  
+
   private func stopTimer() {
     timer?.invalidate()
     timer = nil
   }
 }
-
