63309c607399118fc4db4ddec47620942227dc9e
Foahh <160589889+Foahh@users.noreply.github.com>
2025-11-23 12:28:58 +0800

    fix: update glass effect modifier to use regular style

--- OnlyFoods/Utilities/GlassEffect.swift
+++ OnlyFoods/Utilities/GlassEffect.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct GlassEffectModifier: ViewModifier {
   func body(content: Content) -> some View {
     if #available(iOS 26.0, *) {
-      content.glassEffect()
+      content.glassEffect(.regular)
     } else {
       content
     }
